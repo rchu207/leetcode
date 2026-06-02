43ca5e955bcec9f7135aebb02acbb9a52a605e68
Rafael Chu <rchu207@gmail.com>
2026-06-02 21:17:35 +0800

    1. Two Sum
    - Time: O(n)
    - Space: O(n)
    
    Close rchu207/Learning-2026#119.

--- problems/1/Solution.cpp
+++ problems/1/Solution.cpp
@@ -1,23 +1,21 @@
-#include <vector>
+// 1. Two Sum
+
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-
-        int i, j;
-        for (i = 0; i < nums.size() -1; i++) {
-            for (j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    result.push_back(i);
-                    result.push_back(j);
-                    return result;
-                }
+        unordered_map<int, int> hashmap;
+        for (int i = 0; i < nums.size(); i++) {
+            auto y = target - nums[i];
+            if (hashmap.count(y) > 0) {
+                return {i, hashmap[y]};
+            } else {
+                hashmap[nums[i]] = i;
             }
         }
-
-        return result;
+        return {0, 0};
     }
 };

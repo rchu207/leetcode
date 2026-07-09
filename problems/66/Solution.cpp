0e3987b9d542f260632ccc5f049162e0b90b111c
Rafael Chu <rchu207@gmail.com>
2026-07-09 21:19:50 +0800

    66. Plus One
    Time: O(n)
    Space: O(1)
    
    Close rchu207/Learning-2026#164.

---
+++ problems/66/Solution.cpp
@@ -0,0 +1,20 @@
+// 66. Plus One
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i;
+        for (i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                break;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        if (i < 0) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};

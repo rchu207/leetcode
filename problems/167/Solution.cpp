27d2ee5559de405062e60fe156dbc80b3d0611bb
Rafael Chu <rchu207@gmail.com>
2026-07-13 22:05:27 +0800

    167. Two Sum II - Input Array Is Sorted
    - Time: O(n)
    - Space: O(1)
    
    Close rchu207/Learning-2026#180.

---
+++ problems/167/Solution.cpp
@@ -0,0 +1,20 @@
+// 167. Two Sum II - Input Array Is Sorted
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
+        while (left < right) {
+            auto sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return {left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return {0, 0};
+    }
+};

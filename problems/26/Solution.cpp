d54e28ff0706dcb5ec098166683422ee84becc6c
Rafael Chu <rchu207@gmail.com>
2026-07-16 22:19:55 +0800

    26. Remove Duplicates from Sorted Array
    - Time: O(n)
    - Space: O(1)
    
    Close rchu207/Learning-2026#188.

---
+++ problems/26/Solution.cpp
@@ -0,0 +1,15 @@
+// 26. Remove Duplicates from Sorted Array
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }        
+        return k;
+    }
+};

ee5724a8527209cb66900fe92c32a7dc19c9794c
Rafael Chu <rchu207@gmail.com>
2026-07-16 23:01:27 +0800

    283. Move Zeroes
    - Time: O(n)
    - Space: O(1)
    
    Close rchu207/Learning-2026#189.

---
+++ problems/283/Solution.cpp
@@ -0,0 +1,19 @@
+// 283. Move Zeroes
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        while (left < n) {
+            nums[left] = 0;
+            left++;
+        }
+    }
+};
